d62502574f4b5a566bb4022ed7d6887eb4a2238e
PARAMESHWARI <paramesh.ravi11@gmail.com>
2026-03-24 09:48:38 +0530

    Time: 11 ms (58.33%), Space: 89.6 MB (91.67%) - LeetHub

---
+++ 2906-construct-product-matrix/2906-construct-product-matrix.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[][] constructProductMatrix(int[][] grid) {
+        final int mod=12345;
+        int n=grid.length;
+        int m= grid[0].length;
+        int[][] ans=new int[n][m];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                ans[i][j]=1;
+            }
+        }
+        long mul=1;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                ans[i][j]=(int)(ans[i][j]*mul%mod);
+                mul=mul*grid[i][j]%mod;
+            }
+        }
+        mul=1;
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=m-1;j>=0;j--)
+            {
+                ans[i][j]=(int)(ans[i][j]*mul%mod);
+                mul=mul*grid[i][j]%mod;
+            }
+        }
+        return ans;
+    }
+}
